941408d62f7708aedaea30db7d0d48ea2a09dc42
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-08-04 13:48:00 +0300

    completed

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ function add( a , b){
     return (a += b);
 };
 function subtract(a ,b){
-    return(a -= b)
+    return(a -= b);
 }
 function multiply(a , b){
-    return (a *= b)
+    return (a *= b);
 }
 function divide(a ,b){
-    return (a /= b)
+    return (a /= b);
 }
 function increment(n){
-    return(++n)
+    return(++n);
 }
 function decrement(n){
-    return(--n)
+    return(--n);
 }
 function makeInt(n){
-    return(parseInt(n , 10))
+    return(parseInt(n , 10));
 }
 function preserveDecimal(n){
-    return(parseFloat(n))
+    return(parseFloat(n));
 }
